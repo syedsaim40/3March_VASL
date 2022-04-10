0033c96d42eff339fc66e7fedfd4eebe37258de7
Muhammadhamza <syedsaim40@gmail.com>
2022-04-10 20:27:12 +0500

    order done shukr

--- frontend/src/components/user/loginsugnup.js
+++ frontend/src/components/user/loginsugnup.js
@@ -11,11 +11,6 @@ import {
 } from "../../redux/action/useraction";
 import { useHistory } from "react-router-dom";
 import { useAlert } from "react-alert";
-// import { GoogleLogin } from "react-google-login";
-// import Login from "./googleLogin";
-// import Logout from "./googleLogout";
-// import axios from "axios";
-
 import { GoogleLogin } from "react-google-login";
 
 const LoginSignUp = ({ location }) => {
@@ -57,9 +52,7 @@ const LoginSignUp = ({ location }) => {
     myForm.set("password", password);
     dispatch(register(myForm));
   };
-  // const redirect = location.search
-  //   ? location.search.split("=")[1]
-  //   : "/accounts";
+
   const onSuccess = (response) => {
     console.log(response);
 
@@ -98,32 +91,6 @@ const LoginSignUp = ({ location }) => {
       loginTab.current.classList.add("shiftToLeft");
     }
   };
-
-  // async function handleSubmit(event) {
-  //   event.preventDefault();
-
-  //   try {
-  //     await Auth.signIn(email, password);
-  //     isAuthenticated(true);
-  //     history.push("/");
-  //   } catch (e) {
-  //     alert(e.message);
-  //   }
-  // }
-
-  // const responseSuccessGoogle = (response) => {
-  //   console.log(response)
-  //   axios({
-  //     method: "POST",
-  //     url: "http://localhost:3000",
-  //     data: { tokenId: response.tokenId }
-  //   }).then(response => {
-  //     console.log(response)
-  //   })
-  // }
-
-  // const responseErrorGoogle = (response) => {
-  // }
 
   return (
     <Fragment>
@@ -183,15 +150,6 @@ const LoginSignUp = ({ location }) => {
                   <h3>Or login with</h3>
                 </div>
                 <div className="social_login">
-                  {/* For Google Login Authentication */}
-                  {/* <GoogleLogin
-                    clientId="968709430379-dkv5gov48ieuc3t5kmq5s7in57sri6er.apps.googleusercontent.com"
-                    buttonText="Login google"
-                    onSuccess={responseSuccessGoogle}
-                    onFailure={responseErrorGoogle}
-                    cookiePolicy={"single_host_origin"}
-                    className="btn_primary social_btn"
-                  /> */}
                   <GoogleLogin
                     clientId="1068904748671-ngjq97fgfjtgp3e82efhf4dmd09j4dkf.apps.googleusercontent.com"
                     buttonText="Login With Google"
@@ -200,27 +158,6 @@ const LoginSignUp = ({ location }) => {
                     cookiePolicy={"single_host_origin"}
                     style={{ marginTop: "100px" }}
                   />
-                  {/* <GoogleLogin
-                    clientId="968709430379-dkv5gov48ieuc3t5kmq5s7in57sri6er.apps.googleusercontent.com"
-                    buttonText="Login"
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                    cookiePolicy={'single_host_origin'}
-                  /> */}
-                  {/* <GoogleLogin
-                    clientId="968709430379-dkv5gov48ieuc3t5kmq5s7in57sri6er.apps.googleusercontent.com"
-                    buttonText="Login Facebook"
-                    // onSuccess={responseGoogle}
-                    cookiePolicy={"single_host_origin"}
-                    className="btn_primary social_btn"
-                  /> */}
-                  ,{/* For Facebbok Login Authentication */}
-                  {/* <FacebookLogin
-                      appId="452032583149631"
-                      autoLoad={false}
-                      fields="name,email,picture"
-                      // callback={responseFacebook}
-                    /> */}
                 </div>
                 <div className="group_field form_link">
                   <p onClick={(e) => switchTabs(e, "register")}>
@@ -278,17 +215,6 @@ const LoginSignUp = ({ location }) => {
                     onChange={registerDataChange}
                   />
                 </div>
-                {/* <div className="group_field ">
-                    <label htmlFor="name">
-                      <img src="./Profile.png" alt="Avatar Preview" />
-                    </label>
-                    <input
-                      type="file"
-                      name="avatar"
-                      accept="image/*"
-                      onChange={registerDataChange}
-                    />
-                  </div> */}
 
                 <div className="row group_field e_btn">
                   <button
